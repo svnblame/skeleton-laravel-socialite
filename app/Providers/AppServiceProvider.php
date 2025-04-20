49ca22a8a93d77cd82dc820d76940cefc39037b0
Gene Kelley <phpcodehead@gmail.com>
2025-04-20 18:45:10 -0500

    Configure User migration, factory, and model. User uses ULIDs as primary key, factory uses ->faker instead of faker() helper function, add docblocks to User model to help with static analysis and IDE auto-completion, turn on strict mode for Eloquent models.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
+final class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Model::shouldBeStrict();
     }
 }
